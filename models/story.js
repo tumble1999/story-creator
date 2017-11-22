7e3f4feaf41c2dbf8d1f32d5e4e92f591d3ab608
Tumble <tumblegamer@gmail.com>
2017-11-22 10:59:17 +0000

    fixed length probel for multiple sentences

--- models/story.js
+++ models/story.js
@@ -61,12 +61,14 @@ StorySchema
 
   if (this.text[0]) {
     for (var i = min; i < max; i++) {
-      story[i] = this.text[i].join(" ");
-      if (i<this.currentSentence) {
-        story[i]+=".";
+      if (this.text[i]) {
+        story[i] = this.text[i].join(" ");
+        if (i<this.currentSentence) {
+          story[i]+=".";
+        }
       }
     }
-    return story.join(" ");
+    return story.join(" ") + " ";
   }
   else {
     return "";
